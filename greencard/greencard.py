a1cd5293d8ab1ca77e0e703d180e8d9caec7c2a9
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-08 09:07:43 +1000

    Made test descovery recursive

--- greencard/greencard.py
+++ greencard/greencard.py
@@ -20,16 +20,19 @@ def greencard(func):
 
 def descovery(testdir):
     """Descover and load greencard tests."""
-    from os.path import join, exists, isdir, splitext, basename
+    from os.path import join, exists, isdir, splitext, basename, sep
     if not testdir or not exists(testdir) or not isdir(testdir):
         return None
 
-    from glob import glob
+    from os import walk
+    import fnmatch
     import imp
 
-    for testpath in glob(join(testdir, "*.py")):
-        name, _ = splitext(basename(testpath))
-        imp.load_source('tests.{0}'.format(name), testpath)
+    for root, _, filenames in walk(testdir):
+        for filename in fnmatch.filter(filenames, '*.py'):
+            path = join(root, filename)
+            modulepath = splitext(root)[0].replace(sep, '.')
+            imp.load_source(modulepath, path)
 
 
 RESULTS = """
