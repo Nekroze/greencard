bbf2f2d2d6a8bf7245193ca785127a7c5cf09559
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-07 19:44:00 +1000

    Implemented new decorator and a prototype test runner

--- greencard/decorator.py
+++ greencard/decorator.py
@@ -1,22 +1,51 @@
 from functools import wraps
-from librarian.library import Library
 
 
-class GreenCardTest(object):
+TESTS = []
+
+
+def greencard(func):
     """
     A decorator for providing a unittesting function/method with every card in
     a librarian card library database when it is called.
     """
-    def __init__(self, library):
-        if isinstance(library, Library):
-            self.library = library
-        else:
-            self.library = Library(library)
+    @wraps(func)
+    def Wrapped(*args, **kwargs):
+        """Transparent wrapper."""
+        return func(*args, **kwargs)
+    TESTS.append(Wrapped)
+    return Wrapped
 
-    def __call__(self, func):
-        @wraps(func)
-        def Wrapped(*args, **kwargs):
-            """Give the wrapped function every card."""
-            for card in self.library.retreive_all():
-                func(*args, card=card, **kwargs)
-        return Wrapped
+
+def descovery():
+    """
+    Descover and load all greencard tests.
+    """
+    pass
+
+
+def main(clargs=None):
+    """
+    Command line entry point.
+    """
+    from argparse import ArgumentParser
+    from librarian.library import Library
+
+    parser = ArgumentParser(
+        description="A test runner for each card in a librarian library.")
+    parser.add_argument("library", help="Library database")
+    args = parser.parse_args(clargs)
+
+    descovery()
+
+    lib = Library(args.library))
+    failures = 0
+
+    for card in library.retrieve_all():
+        for test in TESTS:
+            try:
+                test(card)
+            except AssertionError:
+                print("{0} failed {1}".format(card, test.__name__))
+                failures += 1
+    return failure
